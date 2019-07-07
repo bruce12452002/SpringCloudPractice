aa250e8f16d3e7642258f83f9321cdbd2af86a00
xxx <xxx>
2019-07-07 23:36:07 +0800

    finish eureka

--- SpringCloudPractice/provider/src/main/java/main/ProviderMain.java
+++ SpringCloudPractice/provider/src/main/java/main/ProviderMain.java
@@ -2,10 +2,12 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("controller")
+@EnableEurekaClient
 public class ProviderMain {
     public static void main(String[] args) {
         SpringApplication.run(ProviderMain.class, args);
