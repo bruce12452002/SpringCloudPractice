e4565e505971ff5e87b1469f4ab08e6c73031067
xxx <xxx>
2019-07-27 18:59:39 +0800

    增加 zuul

---
+++ SpringCloudPractice/zuul/src/main/java/scp/ZuulMain.java
@@ -0,0 +1,15 @@
+package scp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulMain.class, args);
+    }
+}
