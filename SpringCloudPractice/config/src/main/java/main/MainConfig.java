b0777a440e52e53e46ee5e27ca865b0340e6d5c9
xxx <xxx>
2019-07-28 14:06:27 +0800

    add config server

---
+++ SpringCloudPractice/config/src/main/java/main/MainConfig.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MainConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(MainConfig.class, args);
+    }
+}
