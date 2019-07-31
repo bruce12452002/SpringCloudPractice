1a3225635b2477cb7346a0b054f91ceaf89be9d7
xxx <xxx>
2019-07-31 23:56:10 +0800

    增加 eureka 的 帳號和密碼

--- SpringCloudPractice/eureka/src/main/java/main/EurekaMain.java
+++ SpringCloudPractice/eureka/src/main/java/main/EurekaMain.java
@@ -3,11 +3,20 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaMain {
+public class EurekaMain extends WebSecurityConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // http.csrf().disable(); // 不處理 CSRF 攻擊
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
     }
 }
